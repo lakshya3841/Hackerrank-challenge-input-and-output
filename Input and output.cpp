d423df091c57b7cedff2d61867a33e40247c1724
SaurabhKhade <66903901+SaurabhKhade@users.noreply.github.com>
2020-07-30 20:23:07 +0530

    Add files via upload

---
+++ Input and output.cpp
@@ -0,0 +1,21 @@
+﻿/*
+Name:Saurabh Shankar Khade,
+Annasaheb Dange College of Engineering and Technology, Ashta,
+At:Kagani     Post:Kalkundri
+Tal:Chandagad
+Dist:Kolhapur
+*/
+
+
+//https://www.hackerrank.com/challenges/cpp-input-and-output
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout <<a+b+c;
+    return 0;
+}
