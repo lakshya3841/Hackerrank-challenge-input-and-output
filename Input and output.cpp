9e28e75c78e46383b35390655cd34545fa60c02d
lakshya3841 <62415196+lakshya3841@users.noreply.github.com>
2020-09-30 19:39:21 +0530

    Arethmetic operators at same time

--- Input and output.cpp
+++ Input and output.cpp
@@ -14,8 +14,15 @@ Dist:Kolhapur
 using namespace std;
 int main()
 {
-    int a,b,c;
-    cin>>a>>b>>c;
-    cout <<a+b+c;
+    int a,b;
+    cout<<"Enter 1st number"<<endl;
+    cin>>a;
+    cout<<"Enter 2nd number"<<endl;
+    cin>>b;
+    cout<<" The sum of value given is "<<a+b<<endl;
+    cout<<" The subtraction of value given is "<<a-b<<endl;
+    cout<<" The multiplication of value given is "<<a*b<<endl;
+    cout<<" The division of value given is "<<a/b<<endl;
+    
     return 0;
 }
